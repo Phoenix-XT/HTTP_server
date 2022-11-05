cb8e5efe42f576f5c9f0b266c91fbafd64be5d46
Nzan Christiana <92272635+Phoenix-XT@users.noreply.github.com>
2022-11-05 01:35:28 +0000

    test

--- HTTP_server.js
+++ HTTP_server.js
@@ -5,26 +5,27 @@ app.listen(4000, () => {
     console.log("Application started and Listening on port 4000");
 });
 
-# app.get("/", (req, res) => {
-#     res.send("Testing!");
-# });
-# app.get for method
-# req means request method
-# res means response method
+//app.get("/", (req, res) => {
+    //res.send("Testing!");
+//});
+
+//app.get for method
+//req means request method
+//res means response method
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/AboutMe.html");
+    res.sendFile(__dirname + "/about.html");
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(__dirname + "/ContactMe.html");
+    res.sendFile(__dirname + "/contact.html");
 });
 
-// 404 Page return to home page
+//404 Page return to home page
 app.get("/*", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
